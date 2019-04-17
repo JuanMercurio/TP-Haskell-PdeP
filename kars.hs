c3eeae116157fe3efe5b7fcdad101149e734d193
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2019-04-17 18:23:14 -0300

    Añado .hs para hacer el TP

---
+++ kars.hs
@@ -0,0 +1,6 @@
+module TP where
+
+import Text.Show.Functions -- esto permite que las funciones sean mostrables
+import Data.List -- esto me da la funcion genericLength, usar esta en vez de length
+
+-- Poner aqui la solucion:

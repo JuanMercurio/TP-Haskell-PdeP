d9223a2449507b5cdb5f7391743dcc050cb921f7
Juan Mercurio <juanmercuriobadii@gmail.com>
2019-04-19 16:07:37 -0300

    Punto 1

--- kars.hs
+++ kars.hs
@@ -4,3 +4,56 @@ import Text.Show.Functions -- esto permite que las funciones sean mostrables
 import Data.List -- esto me da la funcion genericLength, usar esta en vez de length
 
 -- Poner aqui la solucion:
+
+type Nombre = String
+type Velocidad = Float 
+type NivelNafta = Float 
+type NombreEnamorade = String
+type Truco = Auto -> Auto
+
+data Auto = UnAuto {
+    nombre :: Nombre, 
+    nivelNafta :: NivelNafta,
+    velocidad :: Velocidad,
+    nombreEnamorade :: NombreEnamorade,
+    trucoFavorito :: Truco
+} deriving (Show)
+
+
+aumentarPorNombre :: Nombre -> Velocidad -> Velocidad
+aumentarPorNombre nombre velocidad = velocidad + genericLength nombre
+
+impresionar :: Auto -> Auto
+impresionar (UnAuto nombre nivelNafta velocidad nombreEnamorade trucoFavorito) =
+    UnAuto nombre  nivelNafta (aumentarPorNombre nombre velocidad) nombreEnamorade trucoFavorito
+
+nitro :: Auto -> Auto
+nitro (UnAuto nombre nivelNafta velocidad nombreEnamorade trucoFavorito) = 
+    UnAuto nombre nivelNafta (velocidad + 15)  nombreEnamorade trucoFavorito 
+    
+fingirAmor :: String -> Auto -> Auto
+fingirAmor nombreNuevoEnamorade auto = 
+   auto {nombreEnamorade = nombreNuevoEnamorade}
+
+pista = 1000
+
+deReversa :: Auto -> Auto
+deReversa (UnAuto nombre  nivelNafta velocidad nombreEnamorade trucoFavorito) =
+    UnAuto nombre (nivelNafta + pista/5) velocidad nombreEnamorade trucoFavorito 
+
+rochaMcQueen :: Auto
+rochaMcQueen = UnAuto "rochaMcQueen" 300 0 "Ronco"  deReversa
+biankerr :: Auto
+biankerr = UnAuto  "biankerr" 500 20 "Tinch"  impresionar
+gushtav :: Auto
+gushtav = UnAuto "gushtav"  200  130 "PetiLaLinda"  nitro
+rodra :: Auto
+rodra = UnAuto "rodra" 0 50 "Taisa" (fingirAmor "gushtav")
+
+-- trucoFavorito rodra 
+
+
+
+
+
+
